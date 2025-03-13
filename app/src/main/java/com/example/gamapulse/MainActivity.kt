13152ec1f2348b3d94d4a9ac3432d48fcab1fadd
nuhgroh2004 <nuhgrohramadani@mail.ugm.ac.id>
2025-03-14 02:12:51 +0700

    Bottom nafigation

---
+++ app/src/main/java/com/example/gamapulse/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.gamapulse
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HomeFragment()).commit()
+
+        // Handling navigation
+        bottomNavView.setOnNavigationItemSelectedListener { item ->
+            val selectedFragment = when (item.itemId) {
+                R.id.home -> HomeFragment()
+                R.id.report -> ReportFragment()
+                R.id.notification -> NotificationFragment()
+                else -> HomeFragment()
+            }
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, selectedFragment).commit()
+            true
+        }
+    }
+}
